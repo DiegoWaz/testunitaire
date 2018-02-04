ab453b1a483048c6babb4692fdc62e30e86af465
Diego <waziri.diego@gmail.com>
2018-02-04 15:39:00 +0100

    [update] homepage

--- application/views/themes/front.php
+++ application/views/themes/front.php
@@ -80,90 +80,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
        <?php echo $output; ?>
     </div>
 
-    <footer id="footer1" class="kode-parallax kode-dark-overlay kode-bg-pattern">
-            <div class="footer-medium">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="about-widget">
-                                <h3>Facebook</h3>
-                                <div class="fb-page" data-href="https://www.facebook.com/LMDPSG/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/LMDPSG/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/LMDPSG/">Le Meilleur du PSG</a></blockquote></div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-4">
-                            <div class="widget widget-flickr kode-gallery-pretty">
-                                <h3>Twiiter</h3>
-                                <a class="twitter-timeline" href="https://twitter.com/LMDPSG">Tweets by LMDPSG</a>
-                                <!-- <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script> -->
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="contact-us-widget">
-                                <h3>Se connecter avec</h3>
-                                <ul class="social-links1">
-                                    <li>
-                                        <a href="#" class="tw-bg1"><i class="fa fa-twitter"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="fb-bg1"><i class="fa fa-facebook"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="youtube-bg1"><i class="fa fa-youtube"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="linkedin-bg1"><i class="fa fa-linkedin"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+    <footer>
+        <div class="kode-bottom-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>© <?php echo date("Y"); ?> LMDNews</p>
                     </div>
-                </div>
-
-                <div class="clearfix clear"></div>
-
-                <div class="pick-event footer-medium kode-pagesection margin-top-30">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="kode-section-title">
-                                    <h2>Les partenaires</h2>
-                                </div>
-                                <div class="kode-partner">
-                                    <ul class="row">
-                                        <li class="col-md-3">
-                                            <a href="#"><img src="<?php echo base_url(); ?>assets/front/images/newsdefault.jpg" alt="partenariat"></a>
-                                        </li>
-                                        <li class="col-md-3">
-                                            <a href="#"><img src="<?php echo base_url(); ?>assets/front/images/newsdefault.jpg" alt="partenariat"></a>
-                                        </li>
-                                        <li class="col-md-3">
-                                            <a href="#"><img src="<?php echo base_url(); ?>assets/front/images/newsdefault.jpg" alt="partenariat"></a>
-                                        </li>
-                                        <li class="col-md-3">
-                                            <a href="#"><img src="<?php echo base_url(); ?>assets/front/images/newsdefault.jpg" alt="partenariat"></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="col-md-6">
+                        <a href="#" id="kode-topbtn" class="thbg-colortwo"><i class="fa fa-angle-up"></i></a>
                     </div>
-                </div>
-            </div>
-    </footer>
-
-    <div class="kode-bottom-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <p>© <?php echo date("Y"); ?> LMDWEB</p>
-                </div>
-                <div class="col-md-6">
-                    <a href="#" id="kode-topbtn" class="thbg-colortwo"><i class="fa fa-angle-up"></i></a>
                 </div>
             </div>
         </div>
-    </div>
+    </footer>
 
     <div class="clearfix clear"></div>
     <script src="<?php echo base_url(); ?>assets/front/js/jquery/jquery.js"></script>
